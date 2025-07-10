4cfebb852b786bc6d8e8dc52cc361097a2b25796
Rakesh Kumar Sahani <nishadrakesh2054@gmail.com>
2025-07-10 14:21:24 +0545

    check backend url

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const BASE_URL = "http://localhost:5000";
+  const BASE_URL = "https://avyabackend.onrender.com";
+//   const BASE_URL = "http://localhost:5000";
 
   const API_URL = `${BASE_URL}/api/photos`;
   const galleryGrid = document.getElementById("gallery-grid");
